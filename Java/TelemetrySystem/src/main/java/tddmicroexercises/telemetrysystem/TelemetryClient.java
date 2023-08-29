8ce5f1b598817d00474171f4f9aafbd21b1b36ef
PreethamKarnekanti <preetham.karnekanti@zemosolabs.com>
2023-08-29 19:05:37 +0530

    added interfaces

--- Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryClient.java
+++ Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryClient.java
@@ -2,17 +2,20 @@ package tddmicroexercises.telemetrysystem;
 
 import java.util.Random;
 
-public class TelemetryClient implements  TelemetrySender
+public class TelemetryClient
 {
     public static final String DIAGNOSTIC_MESSAGE = "AT#UD";
 
     private boolean onlineStatus;
-    private String diagnosticMessageResult = "";
+    private final String diagnosticMessageResult = "";
 
-    private TelemetryConnection connection;
+    private final TelemetryConnection connection;
 
-    public TelemetryClient(TelemetryConnection connection) {
+    private final TelemetryActions telemetryActions;
+
+    public TelemetryClient(TelemetryConnection connection, TelemetryActions telemetryActions) {
         this.connection = connection;
+        this.telemetryActions = telemetryActions;
     }
 
     public boolean getOnlineStatus()
@@ -27,63 +30,17 @@ public class TelemetryClient implements  TelemetrySender
 
     public void disconnect()
     {
-        onlineStatus = false;
+        onlineStatus = connection.disconnect();
     }
 
     public void send(String message)
     {
-        if (message == null || "".equals(message))
-        {
-            throw new IllegalArgumentException();
-        }
-
-        if (message == DIAGNOSTIC_MESSAGE)
-        {
-            // simulate a status report
-            diagnosticMessageResult =
-                  "LAST TX rate................ 100 MBPS\r\n"
-                + "HIGHEST TX rate............. 100 MBPS\r\n"
-                + "LAST RX rate................ 100 MBPS\r\n"
-                + "HIGHEST RX rate............. 100 MBPS\r\n"
-                + "BIT RATE.................... 100000000\r\n"
-                + "WORD LEN.................... 16\r\n"
-                + "WORD/FRAME.................. 511\r\n"
-                + "BITS/FRAME.................. 8192\r\n"
-                + "MODULATION TYPE............. PCM/FM\r\n"
-                + "TX Digital Los.............. 0.75\r\n"
-                + "RX Digital Los.............. 0.10\r\n"
-                + "BEP Test.................... -5\r\n"
-                + "Local Rtrn Count............ 00\r\n"
-                + "Remote Rtrn Count........... 00";
-
-            return;
-        }
-
-        // here should go the real Send operation (not needed for this exercise)
+        telemetryActions.send(message);
     }
 
     public String receive()
     {
-        String message;
-
-        if (diagnosticMessageResult == null || "".equals(diagnosticMessageResult))
-        {
-            // simulate a received message (just for illustration - not needed for this exercise)
-            message = "";
-            int messageLength = connection.getConnectionEventsSimulator().nextInt(50) + 60;
-            for(int i = messageLength; i >=0; --i)
-            {
-                message += (char)connection.getConnectionEventsSimulator().nextInt(40) + 86;
-            }
-            
-        } 
-        else
-        {                
-            message = diagnosticMessageResult;
-            diagnosticMessageResult = "";
-        }
-
-        return message;
+        return telemetryActions.receive();
     }
 }
 
--- Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryConnection.java
+++ Java/TelemetrySystem/src/main/java/tddmicroexercises/telemetrysystem/TelemetryConnection.java
@@ -2,18 +2,25 @@ package tddmicroexercises.telemetrysystem;
 
 import java.util.Random;
 
-public class TelemetryConnection {
+public class TelemetryConnection implements  Connection {
     private final Random connectionEventsSimulator = new Random(42);
 
     public Random getConnectionEventsSimulator() {
         return connectionEventsSimulator;
     }
 
+    @Override
     public boolean establishConnection(String connectionString) {
         if (connectionString == null || "".equals(connectionString)) {
             throw new IllegalArgumentException();
         }
 
         return connectionEventsSimulator.nextInt(10) <= 8;
+    }
+
+    @Override
+    public boolean disconnect()
+    {
+        return false;
     }
 }
